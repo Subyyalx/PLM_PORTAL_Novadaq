80611588e009eec07519f3029943a36ccf3d8d6e
Subyyalx <123625186+Subyyalx@users.noreply.github.com>
2024-12-30 16:29:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class User(BaseModel):
 # Home route
 @app.get("/")
 def home():
-    return {"message": "Welcome to DEV environment!"}
+    return {"message": "Welcome to MASTER!"}
 
 # Get all users
 @app.get("/users")
